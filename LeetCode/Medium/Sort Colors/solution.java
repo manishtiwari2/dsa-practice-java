b5d4a313299d2d6d30544257c10c6e1deb4df572
Manish Tiwari <manishtiwari2578@gmail.com>
2026-08-03 20:20:56 +0530

    [Syncode] LeetCode/Medium/Sort Colors

---
+++ LeetCode/Medium/Sort Colors/solution.java
@@ -0,0 +1,27 @@
+class Solution { 
+    public void sortColors(int[] nums) { 
+        int left = 0; 
+        int mid = 0;
+        int right = nums.length - 1; 
+        
+        while (mid <= right) { 
+            if (nums[mid] == 0) { 
+                swap(nums, left, mid); 
+                left++; 
+                mid++; 
+            } else if (nums[mid] == 1) { 
+                mid++; 
+            } else if (nums[mid] == 2) { 
+                swap(nums, right, mid); 
+                right--; 
+                // check here why not mid++
+            } 
+        } 
+    } 
+
+    private void swap(int[] nums, int i, int j) { 
+        int temp = nums[i]; 
+        nums[i] = nums[j]; 
+        nums[j] = temp; 
+    } 
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 | 2 | [Palindrome Number](./GeeksForGeeks/Easy/Palindrome%20Number) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/palindrome0746/1) | Easy | 03 Aug 2026 | 07:58 pm |
 | 3 | [Array with All Palindromes](./GeeksForGeeks/Basic/Array%20with%20All%20Palindromes) | [GeeksForGeeks](https://www.geeksforgeeks.org/problems/palindromic-array-1587115620/1) | Basic | 03 Aug 2026 | 08:00 pm |
 | 4 | [Move Zeroes](./LeetCode/Easy/Move%20Zeroes) | [LeetCode](https://leetcode.com/problems/move-zeroes/) | Easy | 03 Aug 2026 | 08:10 pm |
+| 5 | [Sort Colors](./LeetCode/Medium/Sort%20Colors) | [LeetCode](https://leetcode.com/problems/sort-colors/) | Medium | 03 Aug 2026 | 08:20 pm |
